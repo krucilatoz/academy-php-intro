462cb3dd3485f6f89f228194eef1e854780464f4
Agung <agung_cz@yahoo.com>
2016-02-19 17:24:48 +0700

    1

--- src/multiplication.php
+++ src/multiplication.php
@@ -1,3 +1,20 @@
 <?php
-
-echo strval(intval($_REQUEST['x']) * intval($_REQUEST['y']));
+$x = $_REQUEST['x'];
+$y = $_REQUEST['y'];
+if (is_numeric($x) && is_numeric($y) && preg_match('/^[-+]?\d+$/',$x) && preg_match('/^[-+]?\d+$/',$y)) {
+    $x = (int)$x;
+    $y = (int)$y;
+    if (is_int($x) && is_int($y)) {
+        $neg = (($x >= 0 && $y >= 0) || ($x < 0 && $y < 0))? "" : "-";
+        $ret = 0;
+        $c = 0;
+        $ax = abs($x); $ay = abs($y);
+        while ($c < $ay) {
+            $ret += $ax;
+            $c++;
+        }
+        echo $neg.$ret;
+    }
+    else echo "";
+}
+else echo "";
